3e4fda8f7380c875c3074f2e02198e8ae52d5626
Salah Adawi <salah.adawi@outlook.com>
2024-11-11 15:40:05 +0200

    add wall collision to vampires

--- src/Vampire.cpp
+++ src/Vampire.cpp
@@ -1,8 +1,8 @@
 #include "Vampire.h"
-// #include "Constants.h"
 #include "Game.h"
 #include "Player.h"
 #include "MathUtils.h"
+#include "Constants.h"
 
 Vampire::Vampire(Game *game, sf::Vector2f position) : m_pGame(game), m_position(position)
 {
@@ -10,15 +10,41 @@ Vampire::Vampire(Game *game, sf::Vector2f position) : m_pGame(game), m_position(
 
 void Vampire::update(float deltaTime)
 {
-    // Player *pPlayer = m_pGame->getPlayer();
-
-    // if (collidesWith(pPlayer))
-    //     pPlayer->setIsDead(true);
-
     sf::Vector2f playerPosition = m_pGame->getPlayer()->getPosition();
     sf::Vector2f direction = VecNormalized(playerPosition - m_position);
-    direction *= VampireSpeed * deltaTime;
-    m_position += direction;
 
-    // printf("Vampire position: %f, %f\n", m_position.x, m_position.y);
+    // Calculate next position
+    sf::Vector2f nextPosition = m_position;
+    float moveAmount = VampireSpeed * deltaTime;
+
+    // Try to move in X direction
+    float nextX = m_position.x + direction.x * moveAmount;
+    if (nextX > 0 && nextX < GridWidth)
+    {
+        if (MapArray1[int(m_position.y) * GridWidth + int(nextX)] < 1)
+        {
+            nextPosition.x = nextX;
+        }
+    }
+
+    // Try to move in Y direction
+    float nextY = m_position.y + direction.y * moveAmount;
+    if (nextY > 0 && nextY < GridHeight)
+    {
+        if (MapArray1[int(nextY) * GridWidth + int(m_position.x)] < 1)
+        {
+            nextPosition.y = nextY;
+        }
+    }
+
+    // Update position
+    m_position = nextPosition;
+
+    // Check for collision with player
+    sf::Vector2f toPlayer = playerPosition - m_position;
+    float distanceToPlayer = sqrt(toPlayer.x * toPlayer.x + toPlayer.y * toPlayer.y);
+    if (distanceToPlayer < 0.5f) // Collision radius
+    {
+        m_pGame->getPlayer()->setIsDead(true);
+    }
 }
--- src/Vampire.h
+++ src/Vampire.h
@@ -1,6 +1,5 @@
 #pragma once
-
-#include "Rectangle.h"
+#include <SFML/System/Vector2.hpp>
 
 class Game;
 
@@ -8,18 +7,10 @@ class Vampire
 {
 public:
     Vampire(Game *game, sf::Vector2f position);
-    virtual ~Vampire() {}
-
     void update(float deltaTime);
-
-    void setIsKilled(bool isKilled) { m_isKilled = isKilled; }
-    bool isKilled() { return m_isKilled; }
-
-    sf::Vector2f getPosition() { return m_position; }
+    sf::Vector2f getPosition() const { return m_position; }
 
 private:
     Game *m_pGame;
-    bool m_isKilled = false;
-
     sf::Vector2f m_position;
 };
